e64be66d29a3e0a6c747518d2c3049eb9a897818
Chen Roth <rothchen@gmail.com>
2020-05-26 22:51:24 +0300

    add counter API

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import expressJwt from 'express-jwt';
+
+import {counterApi} from './routers/counterRouter';
+
 const PORT = 4000;
 
 const {JWT_SECRET = 'secret'} = process.env;
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 // comment out this line if you want to bypass JWT check during development
-app.use(expressJwt({secret: JWT_SECRET}).unless({path: '/'}));
+// app.use(expressJwt({secret: JWT_SECRET}).unless({path: ['', '/']}));
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
 })
+
+app.use('/counter', counterApi);
 
 app.listen(PORT, () => console.log(`Server is up at ${PORT}`));
